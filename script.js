f2b57468471998fa60ecccdffe1d794254254690
nctisz <m.angelodev@gmail.com>
2022-08-11 18:57:09 -0300

    Requisito 16, 17 e 18, além de corrigir tag header no documento

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const inputEmail = document.getElementById('email');
 const inputSenha = document.getElementById('senha');
 const buttonLogin = document.getElementById('login');
-
+const buttonSubmit = document.getElementById('submit-btn');
+const agreement = document.getElementById('agreement');
 function verificaEmailESenha() {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
     alert('Olá, Tryber!');
@@ -10,3 +11,12 @@ function verificaEmailESenha() {
   }
 }
 buttonLogin.addEventListener('click', verificaEmailESenha);
+
+// https://stackoverflow.com/questions/18110865/how-to-disable-enable-a-button-with-a-checkbox-if-checked
+agreement.onchange = function (){
+  if (this.checked){
+    buttonSubmit.disabled = false;
+  } else {
+    buttonSubmit.disabled = true;
+  }
+};
